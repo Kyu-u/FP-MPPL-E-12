e6e3c3cbaf30129a9a08aa559b16ff6610159799
Kyu-u <melanchthonbutarbutar@gmail.com>
2021-12-02 23:03:22 +0700

    edititem, update orderdetails

---
+++ database/migrations/2021_12_02_155807_add_more_to_orderdetails_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMoreToOrderdetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orderdetails', function (Blueprint $table) {
+            $table->string('file')->nullable();
+            $table->string('payment')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orderdetails', function (Blueprint $table) {
+            //
+        });
+    }
+}
