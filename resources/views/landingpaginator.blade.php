e61220951ffc3b497ef15c65ae36b9cff8b06407
Crazy-Newbie <ezie2001@gmail.com>
2021-12-03 01:05:31 +0800

    Paginator update

--- resources/views/landingpaginator.blade.php
+++ resources/views/landingpaginator.blade.php
@@ -1,39 +1,58 @@
 @if ($paginator->hasPages())
     <ul class="pager">
-       
-        @if ($paginator->onFirstPage())
-            <li class="disabled"><span>← Previous</span></li>
-        @else
-            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">← Previous</a></li>
-        @endif
-
-
-      
-        @foreach ($elements as $element)
-           
-            @if (is_string($element))
-                <li class="disabled"><span>{{ $element }}</span></li>
+       <div class="flex mt-4 justify-center">
+            @if ($paginator->onFirstPage())
+            <li class="disabled">
+                <button class="w-10 flex item-center">
+                    <
+                </button>
+            </li>
+            @else
+            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                <button class="w-10 flex item-center">
+                    <
+                </button>
+            </a></li>
             @endif
 
 
+        <div class="flex flex-row items-center px-5 gap-x-5">
+            @foreach ($elements as $element)
            
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
+                    @if (is_string($element))
+                        <li class="disabled"><span>{{ $element }}</span></li>
+                    @endif
+
+                
+            
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
                         <li class="active my-active"><span>{{ $page }}</span></li>
-                    @else
+                        @else
                         <li><a href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
-
+                        @endif
+                    @endforeach
+                        
+                @endif
+            @endforeach
+        </div>
+       
         
         @if ($paginator->hasMorePages())
-            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next →</a></li>
+            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">
+                <button class="w-10 flex item-center">
+                    >
+                </button>
+            </a></li>
         @else
-            <li class="disabled"><span>Next →</span></li>
+            <li class="disabled">
+                <button class="w-10 flex item-center font-bold">
+                    >
+                </button>
+            </li>
         @endif
+        </div>
+        
     </ul>
 @endif 
